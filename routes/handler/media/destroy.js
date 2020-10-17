67631b16fc65a57228f42404cecad47c3a796fdc
Husin <Muhamad.husin0412@gmail.com>
2020-10-17 10:06:12 +0700

    api-gateway

---
+++ routes/handler/media/destroy.js
@@ -0,0 +1,20 @@
+const apiAdatapter = require('../../apiAdapter');
+const { URL_SERVICE_MEDIA } = process.env;
+
+const api = apiAdatapter(URL_SERVICE_MEDIA);
+
+module.exports = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const media = await api.delete(`/media/${id}`);
+        return res.json( media.data )
+    } catch (error) {
+
+        if(error.code === 'ECONNREFUSED') {
+            return res.status(500).json({ status: 'error', message: 'service unavailable'});
+        }
+
+        const { status, data } = error.response;
+        return res.status(status).json(data)
+    }
+}
