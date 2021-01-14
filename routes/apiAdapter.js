96447d3270e06178543f9648bbcaec927eb8718f
Husin <Muhamad.husin0412@gmail.com>
2021-01-14 16:07:36 +0700

    header allowed

--- routes/apiAdapter.js
+++ routes/apiAdapter.js
@@ -4,6 +4,7 @@ const { TIMEOUT } = process.env;
 module.exports = (baseUrl) => {
     return axios.create({
         baseURL: baseUrl,
-        timeout: parseInt(TIMEOUT)
+        timeout: parseInt(TIMEOUT),
+        headers: 'Access-Control-Allow-Origin' = '*'
     })
 }
