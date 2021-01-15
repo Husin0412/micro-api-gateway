05f68b39858351b040a0711875f25303fcd47d72
Husin <Muhamad.husin0412@gmail.com>
2021-01-15 11:32:47 +0700

    api fix

--- routes/apiAdapter.js
+++ routes/apiAdapter.js
@@ -4,7 +4,6 @@ const { TIMEOUT } = process.env;
 module.exports = (baseUrl) => {
     return axios.create({
         baseURL: baseUrl,
-        timeout: parseInt(TIMEOUT),
-        headers: 'Access-Control-Allow-Origin' = '*'
+        timeout: parseInt(TIMEOUT)
     })
 }
